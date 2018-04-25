f1017589c9cc99e29e06a062b24bf2db80df8467
QYG_XXY-PC\QYG_XXY <617659171@qq.com>
2018-04-25 10:09:19 +0800

    no message

--- app/src/main/java/jpush/qyq/com/amapproject/MainActivity.java
+++ app/src/main/java/jpush/qyq/com/amapproject/MainActivity.java
@@ -1,13 +1,75 @@
 package jpush.qyq.com.amapproject;
 
+import com.amap.api.maps.AMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.TextureMapView;
 
+public class MainActivity extends AppCompatActivity {
+    private TextureMapView mapView;
+    private AMap aMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         /*
+         * 设置离线地图存储目录，在下载离线地图或初始化地图设置;
+         * 使用过程中可自行设置, 若自行设置了离线地图存储的路径，
+         * 则需要在离线地图下载和使用地图页面都进行路径设置
+         * */
+        //Demo中为了其他界面可以使用下载的离线地图，使用默认位置存储，屏蔽了自定义设置
+        //  MapsInitializer.sdcardDir =OffLineMapUtils.getSdCacheDir(this);
+
+        mapView = (TextureMapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        init();
     }
+
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
 }
